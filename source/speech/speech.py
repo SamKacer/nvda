0789ee43691ba557288dd09ea5584bcd9f4d347d
Samuel Kacer <samuel.kacer@gmail.com>
2022-04-23 15:05:44 +0100

    Fix E251 unexpected spaces around keyword / parameter equals

--- source/speech/speech.py
+++ source/speech/speech.py
@@ -1160,7 +1160,7 @@ def speakTextInfo(
 		speak(
 			seq,
 			priority=priority,
-			suppressBlanks= suppressBlanks or reason == OutputReason.SAYALL
+			suppressBlanks=suppressBlanks or reason == OutputReason.SAYALL
 		)
 	return speechGen.returnValue
 
